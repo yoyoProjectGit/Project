6b9dc225c567719880ba2bf28b71647b7ca18844
youcef <46266369+yoyoProjectGit@users.noreply.github.com>
2019-05-10 07:26:22 +0100

    test

--- Final Project Form/loginForm.cs
+++ Final Project Form/loginForm.cs
@@ -1,17 +1,10 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace Final_Project_Form
 {
-    public partial class loginForm : Form
+	public partial class loginForm : Form
     {
 		public loginForm()
         {
